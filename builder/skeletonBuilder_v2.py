fbfeb929ca98a724aae1136147f6ec34d45148b6
Pere Balsach <pere.balsach@gmail.com>
2017-02-16 12:14:33 +0100

    finished headClass with spaceSwitch working

--- builder/skeletonBuilder_v2.py
+++ builder/skeletonBuilder_v2.py
@@ -37,16 +37,17 @@ def build():
 	shoulderHelpersList = (side + '_arm0_rigHelper', side + '_arm1_rigHelper')
 	legHelpersList      = pm.ls(side + '_leg*rigHelper')
 	spineHelpersList    = pm.ls('*_spine*rigHelper')
-	handHelperList      = (side + '_arm3_rigHelper', side + '_arm4_rigHelper')
+	handHelperListLeft  = (side + '_arm3_rigHelper', side + '_arm4_rigHelper')
+	handHelperListRight = ('R' + '_arm3_rigHelper', side + '_arm4_rigHelper')
 	footHelperList      = pm.ls(side + '_leg_2_rigHelper', side + '_leg_4_rigHelper', side + '_leg_5_rigHelper')
 
 	armJnts      = generateJoints(side=side, baseName='arm', helperList=armHelpersList, discardLast=0)
 	# shoulderJnts = generateJoints(side=side, baseName='shoulder', helperList=shoulderHelpersList, discardLast=0)
 	legJnts      = generateJoints(side=side, baseName='leg', helperList=legHelpersList, discardLast=3)
 	spineJnts    = generateJoints(side='M', baseName='spine', helperList=spineHelpersList, discardLast=1)
-	handJnts     = generateJoints(side=side, baseName='hand', helperList=handHelperList, discardLast=0)
+	handJnts     = generateJoints(side=side, baseName='hand', helperList=handHelperListLeft, discardLast=0)
 	footJnts     = generateJoints(side=side, baseName='foot', helperList=footHelperList, discardLast=0)
 
-	for jnt in [armJnts[0], legJnts[0], footJnts[0]]:
+	for jnt in [armJnts[0], legJnts[0], footJnts[0], handJnts[0]]:
 		pm.select(jnt)
 		pm.mirrorJoint(mirrorYZ=True, mirrorBehavior=True, searchReplace=("L_", "R_"))
--- rigger/HeadClass.py
+++ rigger/HeadClass.py
@@ -12,15 +12,15 @@ class Head(object):
 		jointList = []
 
 		# check number of spine joints to locate the helper head helpers
-		spineHelpersList = pm.ls('M_spine*rigHelper', type='transform')
+		self.spineHelpersList = pm.ls('M_spine*rigHelper', type='transform')
 
 
 		# get head rigHelpers positions
-		headRoot = pm.PyNode(spineHelpersList[(len(spineHelpersList)-2)])
-		headEnd = pm.PyNode(spineHelpersList[(len(spineHelpersList)-1)])
+		self.headRoot = pm.PyNode(self.spineHelpersList[(len(self.spineHelpersList)-2)])
+		self.headEnd = pm.PyNode(self.spineHelpersList[(len(self.spineHelpersList)-1)])
 
-		self.headRootPos = headRoot.getTranslation('world')
-		self.headEndPos  = headEnd.getTranslation('world')
+		self.headRootPos = self.headRoot.getTranslation('world')
+		self.headEndPos  = self.headEnd.getTranslation('world')
 
 		jointList.append(pm.joint(name='M_headRoot_jnt', p=(self.headRootPos[0], self.headRootPos[1], self.headRootPos[2])))
 		jointList.append(pm.joint(name='M_headEnd_jnt', p=(self.headEndPos[0], self.headEndPos[1], self.headEndPos[2])))
@@ -31,7 +31,8 @@ class Head(object):
 			pm.joint(e=True, oj='yxz', secondaryAxisOrient='zup', ch=True, zso=True)
 
 
-	def createneckCtrl(self):
+	def createNeckCtrl(self):
+
 		headCtrl = rigUtils.createRigControl('circle')
 		headCtrl = pm.rename(headCtrl[0], 'M_head_ctrl')
 		rigUtils.setControlColor(headCtrl)
@@ -41,34 +42,68 @@ class Head(object):
 		pm.scale(headCtrl, (1.5, 1.5, 1.5))
 		pm.makeIdentity(a=True, r=True, t=True, s=True)
 
+
 	def headSetup(self):
 		pm.parent('M_headRoot_jnt', 'M_head_ctrl')
+		pm.pointConstraint('M_head_ctrl', self.headRoot )
+
 
 	def spaceSwitchHeadSetup(self):
+
 		mainFollowLoc = pm.spaceLocator(name='M_headFollowMain_loc')
 		headFollowLoc = pm.spaceLocator(name='M_headFollowHead_loc')
 
 		# reduce local scale
 		for loc in [mainFollowLoc, headFollowLoc]:
-			pm.setAttr(loc + 'Shape.localScaleX', 0.0015)
-			pm.setAttr(loc + 'Shape.localScaleY', 0.0015)
-			pm.setAttr(loc + 'Shape.localScaleZ', 0.0015)
+			pm.setAttr(loc + 'Shape.localScaleX', 0.015 )
+			pm.setAttr(loc + 'Shape.localScaleY', 0.015)
+			pm.setAttr(loc + 'Shape.localScaleZ', 0.015)
 
 		mainFollowGrp = pm.group(name='M_headFollowMain_grp', em=True)
 		headFollowGrp = pm.group(name='M_headFollowHead_grp', em=True)
 
 		pm.parent(mainFollowLoc, mainFollowGrp)
+		pm.parent(mainFollowGrp, 'main_ctrl')
+
 		pm.parent(headFollowLoc, headFollowGrp)
+		# pm.parent(headFollowGrp, 'M_head_ctrl')
+
 
 		pm.move(mainFollowGrp, (self.headRootPos[0], self.headRootPos[1], self.headRootPos[2]))
 		pm.move(headFollowGrp, (self.headRootPos[0], self.headRootPos[1], self.headRootPos[2]))
 
-		pm.parent('M_head_ctrl', 'main_ctrl')
+		neckCtrlNum = (len(pm.ls('M_spine*_ctrl')))
+		pm.select('M_spine%s_ctrl' %neckCtrlNum)
+		neckCtrl = pm.ls(sl=True)[0]
+		neckCtrl = pm.PyNode(neckCtrl)
+		neckPos = neckCtrl.getTranslation('world')
+
+		headFollowHeadGrp = pm.group(n='M_followHead_grp')
+		pm.parent(headFollowGrp ,headFollowHeadGrp)
+
+		pm.xform(headFollowGrp, piv=neckPos, ws=True)
+
+		followOrientConst = pm.orientConstraint('M_headFollowMain_loc', 'M_headFollowHead_loc', headFollowHeadGrp)
+		print ('NeckCtrl: %s' %neckCtrl)
+		pm.parent('M_head_ctrl', neckCtrl)
+
+		rigUtils.hideAttributes(ctrl='M_head_ctrl', trans=True, scale=True, rot=False, vis=True, radius=False)
+
+		# add channelbox attributes to switch space
+		pm.addAttr('M_head_ctrl', ln="follow", at="enum", en="main:head:" )
+		pm.setAttr('M_head_ctrl.follow', e=True, keyable=True)
+
+		# create connections from the follow attrs to the orientConstraint
+		headFollowInvNode = pm.createNode('reverse', name='headFollow_inv')
+
+		pm.connectAttr('M_head_ctrl.follow', 'M_followHead_grp_orientConstraint1.M_headFollowHead_locW1', f=True)
+		pm.connectAttr('M_head_ctrl.follow', headFollowInvNode + '.inputX', f=True)
+		pm.connectAttr(headFollowInvNode + '.outputX', 'M_followHead_grp_orientConstraint1.M_headFollowMain_locW0',  f=True)
 
 
 	def _build(self):
 		self.createHeadJoints()
-		self.createneckCtrl()
+		self.createNeckCtrl()
 		self.headSetup()
 		self.spaceSwitchHeadSetup()
 
